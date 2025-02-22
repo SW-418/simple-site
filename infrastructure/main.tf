8ff6f3f59967bc60ddc590884c5d0c0cfa510ef3
Sam Wells <samwells95@gmail.com>
2025-02-21 18:15:41 -0800

    Define S3 bucket

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,3 +10,17 @@ terraform {
 provider "aws" {
   region = "ca-central-1"
 }
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "simple-site"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
