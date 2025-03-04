d5945c33ff182590eee3b97bef581540f398064c
Sam Wells <samwells95@gmail.com>
2025-03-03 21:09:48 -0800

    Improve formatting a bit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <div className="flex items-center space-x-4">
-              <div>
-                  <Image src="/sw.jpg" alt="profile picture" width={200} height={200}/>
+      <main className="flex flex-col row-start-2 items-center">
+          <div className="flex items-center space-x-6">
+              <div className="">
+                  <Image src="/sw.jpg" alt="profile picture" width={200} height={200} className="rounded-xl" />
               </div>
-              <div>
+              <div className="text-sm w-3/4">
                   <h1>Hi - I&apos;m Sam 👋</h1>
-                  <h1>I&apos;m a Software Engineer predominantly focused on backend development 👨🏽‍💻</h1>
+                  <p>I&apos;m a Software Engineer predominantly focused on backend development 👨🏽‍💻</p>
               </div>
           </div>
       </main>
