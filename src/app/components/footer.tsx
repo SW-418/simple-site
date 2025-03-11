63f9fdc50b12ca51293f1b4aefa3afb3f0764b8e
Sam Wells <samwells95@gmail.com>
2025-03-11 16:21:43 -0700

    Extract footer

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="flex justify-center text-sm self-end">
+            Created and maintained by ya boi
+        </footer>
+    );
+}
