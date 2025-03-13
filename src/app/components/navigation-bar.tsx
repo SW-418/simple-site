d7192081a3c6ab60ef515426f5adb60edfcf532b
Sam Wells <samwells95@gmail.com>
2025-03-13 10:09:09 -0700

    Formatting improvements (#41)
    * Improve base page layout
    
    * Nav bar layout improvement

--- src/app/components/navigation-bar.tsx
+++ src/app/components/navigation-bar.tsx
@@ -11,17 +11,19 @@ interface NavigationBarProps {
 export default function NavigationBar({ navigatedPage = "/", navigatedPageText = "Home" }: NavigationBarProps) {
     return (
         <div className="flex flex-row justify-center">
-        <div className="bg-neutral-600 p-1.5 rounded-xl">
-            <div className="flex space-x-3 pb-2">
-                <ImageButton logo="/gh.png" alt="Github Profile SW-418" url="https://github.com/SW-418" size={50} />
-                <ImageButton logo="/leetcode.png" alt="Leetcode Profile" url="https://leetcode.com/u/samwells95" size={50} />
-                <ImageButton logo="/insta.png" alt="Instagram Profile" url="https://www.instagram.com/__sam.wells" size={50} />
-                <ImageButton logo="/linkedin.png" alt="LinkedIn Profile" url="https://www.linkedin.com/in/sw-418" size={50} />
-            </div>
-            <div className="bg-black p-1 rounded-xl flex justify-center hover:bg-neutral-00">
-                <Link href={navigatedPage}>{navigatedPageText}</Link>
+            <div className="flex flex-col">
+                <div className="bg-neutral-600 p-1.5 rounded-xl">
+                    <div className="flex space-x-3 pb-2">
+                        <ImageButton logo="/gh.png" alt="Github Profile SW-418" url="https://github.com/SW-418" size={50} />
+                        <ImageButton logo="/leetcode.png" alt="Leetcode Profile" url="https://leetcode.com/u/samwells95" size={50} />
+                        <ImageButton logo="/insta.png" alt="Instagram Profile" url="https://www.instagram.com/__sam.wells" size={50} />
+                        <ImageButton logo="/linkedin.png" alt="LinkedIn Profile" url="https://www.linkedin.com/in/sw-418" size={50} />
+                    </div>
+                    <div className="bg-black p-1 rounded-xl flex justify-center hover:bg-neutral-00">
+                        <Link href={navigatedPage}>{navigatedPageText}</Link>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import Footer from "@/app/components/footer";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-1 grid-rows-[auto_1fr_auto] min-h-[100dvh] h-[100dvh] font-[family-name:var(--font-geist-sans)] p-8">
+    <div className="grid grid-cols-1 grid-rows-[1fr_1fr_1fr] min-h-[100dvh] h-[100dvh] font-[family-name:var(--font-geist-sans)] p-8">
       <NavigationBar navigatedPage="/photography" navigatedPageText="Photography" />
       <main className="flex flex-col items-center justify-center">
           <div className="flex justify-center items-center space-x-7">
-              <Image src="/sw.jpg" alt="profile picture" width={100} height={100} className="rounded-xl" />
-              <div className="text-sm text-left">
+              <Image src="/sw.jpg" alt="profile picture" width={125} height={125} className="rounded-xl" />
+              <div className="text-sm md:text-base text-left">
                   <h1>Hi - I&apos;m Sam 👋</h1>
                   <p>I&apos;m a Software Engineer predominantly focused on backend development 👨🏽‍💻</p>
               </div>
