1af07f735b2b13b6d0a50e4fc0a14c42b465c89e
Sam Wells <samwells95@gmail.com>
2025-03-16 10:37:46 -0700

    Improve page style and sizing

--- src/app/photography/page.tsx
+++ src/app/photography/page.tsx
@@ -4,12 +4,12 @@ import Footer from "../components/footer";
 
 export default function Photography() {
     return (
-        <div className="min-h-[100dvh] font-[family-name:var(--font-geist-sans)] flex flex-col">
+        <div className="min-h-[100dvh] font-[family-name:var(--font-geist-sans)] flex flex-col w-[90%] md:w-[80%] xl:w-[70%] 2xl:w-[60%] mx-auto">
           <div className="sticky top-0 z-10 pt-4">
             <NavigationBar navigatedPage="/" navigatedPageText="Home" />
           </div>
-          <main className="flex-1 px-8 py-8">
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-7xl mx-auto">
+          <main className="flex-1 pt-4">
+              <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8">
                 <ImageWithDescription image="/mamab.jpg" description="Mama Bear - Toba Inlet" />
                 <ImageWithDescription image="/eclipse.jpg" description="Lunar Eclipse 2025" />
                 <ImageWithDescription image="/lake.jpg" description="Johnson Lake" />
