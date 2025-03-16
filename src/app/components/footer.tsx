3e34c1ccc448ec6617bccf9f1268bb8563e07b79
Sam Wells <samwells95@gmail.com>
2025-03-16 10:36:54 -0700

    Set footer txt color

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="bg-white/5 backdrop-blur-sm px-4 py-2 rounded-lg text-sm w-fit mx-auto">
+        <footer className="text-white bg-white/5 backdrop-blur-sm px-4 py-2 rounded-lg text-sm w-fit mx-auto">
             Created and maintained by ya boi
         </footer>
     );
