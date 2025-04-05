058fc66e1ab008fe50e30c26643bd68a024c83f2
Sam Wells <samwells95@gmail.com>
2025-04-05 16:09:48 -0700

    Add new images

--- src/app/photography/page.tsx
+++ src/app/photography/page.tsx
@@ -12,15 +12,24 @@ export default function Photography() {
                 <ImageWithDescription image="/mamab.jpg" description="Mama Bear - Toba Inlet" />
                 <ImageWithDescription image="/eclipse.jpg" description="Lunar Eclipse 2025" />
                 <ImageWithDescription image="/lake.jpg" description="Johnson Lake" />
-                <ImageWithDescription image="/tip.jpg" description="Mount Girouard - Banff" />
+                <ImageWithDescription image="/rainbow.jpg" description="Rainbow - Near Prince Rupert" />
+                <ImageWithDescription image="/sea.jpg" description="Void Check - Near Prince Rupert" />
+                <ImageWithDescription image="/port.jpg" description="Port - Near Prince Rupert" />
+                <ImageWithDescription image="/falls1.jpg" description="Stokes Creek Falls" />
+                <ImageWithDescription image="/falls2.jpg" description="Little Qualicum Falls Provincial Park" />
+                <ImageWithDescription image="/elk.jpg" description="Diabolical Sideye from an Elk - Youbou" />
                 <ImageWithDescription image="/heron.jpg" description="Heron - Lost Lagoon" />
+                <ImageWithDescription image="/chipmunk.jpg" description="Neotamias - Secret Cove" />
                 <ImageWithDescription image="/inukshuk.jpg" description="Inukshuk - Vancouver Seawall" />
+                <ImageWithDescription image="/tip.jpg" description="Mount Girouard - Banff" />
                 <ImageWithDescription image="/coyote.jpg" description="Coyote - Near Kamloops" />
                 <ImageWithDescription image="/eagle.jpg" description="Eagle - UBC" />
                 <ImageWithDescription image="/sunset.jpg" description="Sunset" />
                 <ImageWithDescription image="/dahlia.jpg" description="Dahlia - Butchart Gardens" />
                 <ImageWithDescription image="/ice.jpg" description="Icy" />
                 <ImageWithDescription image="/camp.jpg" description="Backcountry Camp - Lindeman Lake" />
+                <ImageWithDescription image="/train.jpg" description="Train - Between Terrace and Prince Rupert" />
+                <ImageWithDescription image="/boats.jpg" description="Boats - Powell Lake" />
               </div>
           </main>
         </div>
