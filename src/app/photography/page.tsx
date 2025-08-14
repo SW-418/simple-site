58a04c62f24fa0a1d13fe0f238fb483f3c0e45ee
Sam Wells <samwells95@gmail.com>
2025-08-13 23:06:48 -0700

    Add new pictures

--- src/app/photography/page.tsx
+++ src/app/photography/page.tsx
@@ -12,18 +12,27 @@ export default function Photography() {
                 <ImageWithDescription image="/mamab.jpg" description="Mama Bear - Toba Inlet" />
                 <ImageWithDescription image="/eclipse.jpg" description="Lunar Eclipse 2025" />
                 <ImageWithDescription image="/lake.jpg" description="Johnson Lake" />
+                <ImageWithDescription image="/breach.jpg" description="Breach - Mayne Island" />
+                <ImageWithDescription image="/snake.jpg" description="Snake - Mayne Island" />
+                <ImageWithDescription image="/deer.jpg" description="Deer - Mayne Island" />
                 <ImageWithDescription image="/rainbow.jpg" description="Rainbow - Near Prince Rupert" />
                 <ImageWithDescription image="/sea.jpg" description="Void Check - Near Prince Rupert" />
                 <ImageWithDescription image="/port.jpg" description="Port - Near Prince Rupert" />
                 <ImageWithDescription image="/falls1.jpg" description="Stokes Creek Falls" />
                 <ImageWithDescription image="/falls2.jpg" description="Little Qualicum Falls Provincial Park" />
                 <ImageWithDescription image="/elk.jpg" description="Diabolical Sideye from an Elk - Youbou" />
+                <ImageWithDescription image="/stars1.jpg" description="Seein' Stars I - Mayne Island" />
+                <ImageWithDescription image="/stars2.jpg" description="Seein' Stars II - Mayne Island" />
+                <ImageWithDescription image="/stars3.jpg" description="Seein' Stars III - Mayne Island" />
                 <ImageWithDescription image="/heron.jpg" description="Heron - Lost Lagoon" />
                 <ImageWithDescription image="/chipmunk.jpg" description="Neotamias - Secret Cove" />
                 <ImageWithDescription image="/inukshuk.jpg" description="Inukshuk - Vancouver Seawall" />
                 <ImageWithDescription image="/tip.jpg" description="Mount Girouard - Banff" />
                 <ImageWithDescription image="/coyote.jpg" description="Coyote - Near Kamloops" />
                 <ImageWithDescription image="/eagle.jpg" description="Eagle - UBC" />
+                <ImageWithDescription image="/sunfire1.jpg" description="Sunfire I - Mayne Island" />
+                <ImageWithDescription image="/sunfire2.jpg" description="Sunfire II - Mayne Island" />
+                <ImageWithDescription image="/sunfire3.jpg" description="Sunfire III - Mayne Island" />
                 <ImageWithDescription image="/sunset.jpg" description="Sunset" />
                 <ImageWithDescription image="/dahlia.jpg" description="Dahlia - Butchart Gardens" />
                 <ImageWithDescription image="/ice.jpg" description="Icy" />
