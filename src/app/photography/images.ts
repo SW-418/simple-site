a9c79ec66101c183b4833246ece359d22f708a00
Sam Wells <samwells95@gmail.com>
2025-08-13 23:14:49 -0700

    Extract image metadata + improve

---
+++ src/app/photography/images.ts
@@ -0,0 +1,37 @@
+export interface PhotoData {
+  src: string;
+  description: string;
+}
+
+export const photos: PhotoData[] = [
+  { src: "/mamab.jpg", description: "Mama Bear - Toba Inlet" },
+  { src: "/eclipse.jpg", description: "Lunar Eclipse 2025" },
+  { src: "/lake.jpg", description: "Johnson Lake" },
+  { src: "/breach.jpg", description: "Breach - Mayne Island" },
+  { src: "/snake.jpg", description: "Snake - Mayne Island" },
+  { src: "/deer.jpg", description: "Deer - Mayne Island" },
+  { src: "/rainbow.jpg", description: "Rainbow - Near Prince Rupert" },
+  { src: "/sea.jpg", description: "Void Check - Near Prince Rupert" },
+  { src: "/port.jpg", description: "Port - Near Prince Rupert" },
+  { src: "/falls1.jpg", description: "Stokes Creek Falls" },
+  { src: "/falls2.jpg", description: "Little Qualicum Falls Provincial Park" },
+  { src: "/elk.jpg", description: "Diabolical Sideye from an Elk - Youbou" },
+  { src: "/stars1.jpg", description: "Seein' Stars I - Mayne Island" },
+  { src: "/stars2.jpg", description: "Seein' Stars II - Mayne Island" },
+  { src: "/stars3.jpg", description: "Seein' Stars III - Mayne Island" },
+  { src: "/heron.jpg", description: "Heron - Lost Lagoon" },
+  { src: "/chipmunk.jpg", description: "Neotamias - Secret Cove" },
+  { src: "/inukshuk.jpg", description: "Inukshuk - Vancouver Seawall" },
+  { src: "/tip.jpg", description: "Mount Girouard - Banff" },
+  { src: "/coyote.jpg", description: "Coyote - Near Kamloops" },
+  { src: "/eagle.jpg", description: "Eagle - UBC" },
+  { src: "/sunfire1.jpg", description: "Sunfire I - Mayne Island" },
+  { src: "/sunfire2.jpg", description: "Sunfire II - Mayne Island" },
+  { src: "/sunfire3.jpg", description: "Sunfire III - Mayne Island" },
+  { src: "/sunset.jpg", description: "Sunset" },
+  { src: "/dahlia.jpg", description: "Dahlia - Butchart Gardens" },
+  { src: "/ice.jpg", description: "Icy" },
+  { src: "/camp.jpg", description: "Backcountry Camp - Lindeman Lake" },
+  { src: "/train.jpg", description: "Train - Between Terrace and Prince Rupert" },
+  { src: "/boats.jpg", description: "Boats - Powell Lake" }
+];
