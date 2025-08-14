5568f923051e8dcc0b4bb833f47e86967dedd7d4
Sam Wells <samwells95@gmail.com>
2025-08-13 23:30:36 -0700

    Up image sizes

--- src/app/components/image-with-description.tsx
+++ src/app/components/image-with-description.tsx
@@ -10,7 +10,7 @@ interface ImageWithDescriptionProps {
 export default function ImageWithDescription({ image, description }: ImageWithDescriptionProps) {
     return (
         <div className="flex flex-col items-center justify-center">
-            <Image src={image} alt={description} width={500} height={500} className="rounded-xl" />
+            <Image src={image} alt={description} width={650} height={650} className="rounded-xl" />
             <p>{description}</p>
         </div>
     );
